a39046b8f77f639699949806adb640be21d83fe6
Ludvij <Luisvijande@gmail.com>
2022-02-15 11:39:42 +0100

    added body to while

--- src/ast/statements/If.java
+++ src/ast/statements/If.java
@@ -2,25 +2,33 @@ package ast.statements;
 
 import ast.AbstractASTNode;
 import ast.Expression;
+import ast.Statement;
 
 import java.util.List;
 
-public class If extends AbstractASTNode implements Expression {
+public class If extends AbstractASTNode implements Statement {
 
-    private final List<Expression> ifBody;
-    private final List<Expression> elseBody;
+    private final List<Statement> ifBody;
+    private final List<Statement> elseBody;
+    private final Expression expression;
 
-    public If(List<Expression> ifBody, List<Expression> elseBody, int column, int line) {
+    public If(Expression expression, List<Statement> ifBody, List<Statement> elseBody, int column, int line) {
         super(column, line);
+
+        this.expression = expression;
         this.ifBody = ifBody;
         this.elseBody = elseBody;
     }
 
-    public List<Expression> getIfBody() {
+    public Expression getExpression() {
+        return expression;
+    }
+
+    public List<Statement> getIfBody() {
         return ifBody;
     }
 
-    public List<Expression> getElseBody() {
+    public List<Statement> getElseBody() {
         return elseBody;
     }
 }
--- src/ast/statements/While.java
+++ src/ast/statements/While.java
@@ -4,15 +4,25 @@ import ast.AbstractASTNode;
 import ast.Expression;
 import ast.Statement;
 
+import java.util.List;
+
 public class While extends AbstractASTNode implements Statement {
 
     private final Expression innerExpression;
+    private final List<Statement> body;
 
-    public While(Expression innerExpression, int column, int line) {
+    public While(Expression innerExpression, List<Statement> body, int column, int line) {
         super(column, line);
         this.innerExpression = innerExpression;
+        this.body = body;
     }
 
-    public Expression getInnerExpression() { return innerExpression; }
+    public Expression getInnerExpression() {
+         return innerExpression; 
+    }
+    
+    public List<Statement> getBody() {
+        return body;
+    }
 
 }
